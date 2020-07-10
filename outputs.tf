6e8b63d69c6d433afa155cc702248d0d86547364
Chris Smith <csmith@hashicorp.com>
2020-07-10 09:16:04 -0500

    changed output of network data to eips

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vault_ip_addr" {
-  value = aws_instance.vault.public_ip
+  value = aws_eip.vault.public_ip
 }
 output "vault_dns" {
-  value = aws_instance.vault.public_dns
+  value = aws_eip.vault.public_dns
 }
 output "app_ip_addr" {
-  value = aws_instance.application.public_ip
+  value = aws_eip.application.public_ip
 }
 output "app_dns" {
-  value = aws_instance.application.public_dns
+  value = aws_eip.application.public_dns
 }
 output "db_endpoint" {
   value = aws_db_instance.football_db.endpoint
